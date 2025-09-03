9bc65e940c7f411ef48bb6f542baf519a2e426d0
Moubeen Akram <moubeenakram@gmail.com>
2025-09-03 16:11:45 +0500

    main.py

--- main.py
+++ main.py
@@ -33,18 +33,27 @@ def press_button(command):
         entry.insert(tk.END,"0")
     elif(command=='+'):
         entry.insert(tk.END,"+")
-    elif(command=="/"):
+    elif(command=="÷ "):
         entry.insert(tk.END,"/")
     elif(command=='-'):
         entry.insert(tk.END,"-")
-    elif(command=='*'):
+    elif(command=='x'):
         entry.insert(tk.END,"*")
+    #to delete last entry
+    elif(command=="⌫"):
+        current=entry.get()
+        new=current[:-1]
+        entry.delete(0,tk.END)
+        entry.insert(0,new)
+
+    elif(command=="."):
+        entry.insert(tk.END,".")
 
 #equal command to perform operation of equal
 def equal(command):
-    command=entry.get()
+    comm=entry.get()
     try:
-        result=eval(command)
+        result=eval(comm)
         entry.delete(0,tk.END)
         entry.insert(0,result)
     except:
@@ -60,39 +69,49 @@ def clear(command):
 #for buttons of the calculator
 #Lambda creates an anonymous function that delays execution until the button is clicked
 
-button1=tk.Button(width=2,text="1",command=lambda: press_button("1"))
-button2=tk.Button(width=2,text="2",command=lambda: press_button("2"))
-button3=tk.Button(width=2,text="3",command=lambda: press_button("3"))
-button4=tk.Button(width=2,text="4",command=lambda: press_button("4"))
-button5=tk.Button(width=2,text="5",command=lambda: press_button("5"))
-button6=tk.Button(width=2,text="6",command=lambda: press_button("6"))
-button7=tk.Button(width=2,text="7",command=lambda: press_button("7"))
-button8=tk.Button(width=2,text="8",command=lambda: press_button("8"))
-button9=tk.Button(width=2,text="9",command=lambda: press_button("9"))
-button10=tk.Button(width=2,text="0",command=lambda: press_button("0"))
-button11=tk.Button(width=2,text="/",command=lambda: press_button("/"))
-button12=tk.Button(width=2,text="x",command=lambda:press_button("x"))
-button13=tk.Button(width=2,text="-",command=lambda:press_button('-'))
-button_plus=tk.Button(width=1,text="+",command=lambda:press_button('+'))
-button_dot=tk.Button(width=1,text='C',command=lambda:clear('C'))
-button_eq=tk.Button(width=1,text='=',command=lambda: equal("="))
-button7.grid(row=1,column=0,sticky="nsew") #sticky is used for north,south,east west
-button8.grid(row=1,column=1,sticky="nsew")
-button9.grid(row=1,column=2,sticky="nsew")
-button11.grid(row=1,column=3,sticky="nsew")
-button4.grid(row=2,column=0,sticky="nsew")
-button5.grid(row=2,column=1,sticky="nsew")
-button6.grid(row=2,column=2,sticky="nsew")
+button1=tk.Button(width=2,text="1",command=lambda: press_button("1"),font="bold")
+button2=tk.Button(width=2,text="2",command=lambda: press_button("2"),font="bold")
+button3=tk.Button(width=2,text="3",command=lambda: press_button("3"),font="bold")
+button4=tk.Button(width=2,text="4",command=lambda: press_button("4"),font="bold")
+button5=tk.Button(width=2,text="5",command=lambda: press_button("5"),font="bold")
+button6=tk.Button(width=2,text="6",command=lambda: press_button("6"),font="bold")
+button7=tk.Button(width=2,text="7",command=lambda: press_button("7"),font="bold")
+button8=tk.Button(width=2,text="8",command=lambda: press_button("8"),font="bold")
+button9=tk.Button(width=2,text="9",command=lambda: press_button("9"),font="bold")
+button10=tk.Button(width=2,text="0",command=lambda: press_button("0"),font="bold")
+button11=tk.Button(width=2,text="÷ ",command=lambda: press_button("÷ "),font="bold")
+button12=tk.Button(width=2,text="x",command=lambda:press_button("x"),font="bold")
+button13=tk.Button(width=2,text="-",command=lambda:press_button('-'),font="bold")
+button_plus=tk.Button(width=1,text="+",command=lambda:press_button('+'),font="bold")
+button_dot=tk.Button(width=1,text='C',command=lambda:clear('C'),font="bold")
+button_eq=tk.Button(width=1,text='=',command=lambda: equal("="),font="bold")
+button_point=tk.Button(width=1,text=".",command=lambda: press_button("."),font="bold")
+button_backspace=tk.Button(width=1,text="⌫",command=lambda: press_button("⌫"),font="bold")
+
+#sticky is used for north,south,east west
+button7.grid(row=2,column=0,sticky="nsew") 
+button8.grid(row=2,column=1,sticky="nsew")
+button9.grid(row=2,column=2,sticky="nsew")
 button12.grid(row=2,column=3,sticky="nsew")
-button1.grid(row=3,column=0,sticky="nsew")
-button2.grid(row=3,column=1,sticky="nsew")
-button3.grid(row=3,column=2,sticky="nsew")
+
+button4.grid(row=3,column=0,sticky="nsew")
+button5.grid(row=3,column=1,sticky="nsew")
+button6.grid(row=3,column=2,sticky="nsew")
 button13.grid(row=3,column=3,sticky="nsew")
-button10.grid(row=4,column=0,sticky="nsew")
-button_dot.grid(row=4,column=1,sticky="nsew")
-button_eq.grid(row=4,column=2,sticky="nsew")
+
+button1.grid(row=4,column=0,sticky="nsew")
+button2.grid(row=4,column=1,sticky="nsew")
+button3.grid(row=4,column=2,sticky="nsew")
 button_plus.grid(row=4,column=3,sticky="nsew")
 
+button10.grid(row=5,column=0,sticky="nsew")
+button_eq.grid(row=5,column=2,sticky="nsew",columnspan=2)
+button_point.grid(row=5,column=1,sticky="nsew")
+
+#for backspace or deleting the last entry
+button_dot.grid(row=1,column=0,sticky="nsew",columnspan=2)
+button_backspace.grid(row=1,column=2,sticky="nsew")
+button11.grid(row=1,column=3,sticky="nsew")
 
 #to show the screen
 window.geometry("300x200")
